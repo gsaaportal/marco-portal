d96f912c4f0439c04cae5f195799b8e8fa27bee2
Tim Glaser <tim.glaser@ecotrust.org>
2013-04-22 15:22:11 -0700

    updated the fabfile and requirements

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,57 @@
-from fabric.api import * 
+from fabric.api import *
 
-dev_server =  'ubuntu@dev.marco.marineplanning.org:22'
-prod_server = []
+vars = {
+    'app_dir': '/usr/local/apps/geosurvey/server',
+    'venv': '/usr/local/apps/geosurvey/geosurvey_env'
+}
 
 env.forward_agent = True
-env.key_filename = '~/.ssh/marco_dsa'
 
-def dev(): 
-   """ Use development server settings """
-   env.hosts = [dev_server]
-   
+
+
+def dev():
+    """ Use development server settings """
+    servers = ['vagrant@127.0.0.1:2222']
+    env.hosts = servers
+    env.key_filename = '~/.vagrant.d/insecure_private_key'
+    vars['app_dir'] = '/vagrant/marco'
+    vars['venv'] = '/usr/local/venv/marco'
+    return servers
+
+
 def prod():
-   """ Use production server settings """ 
-   env.hosts = [prod_server] 
-   
-   
-def all(): 
-   """ Use all serves """
-   env.hosts = [dev_server, prod_server]
+    """ Use production server settings """
+    servers = []
+    env.hosts = servers
+    return servers
 
 
-def install():
-	run('cd /usr/local/apps/marco/marco-portal/ && /vagrant/marco/manage.py --noinput syncdb && /usr/local/venv/marco/bin/python /vagrant/marco/manage.py migrate')
+def test():
+    """ Use test server settings """
+    servers = ['dionysus']
+    env.hosts = servers
+    return servers
 
-def update(): 
-   run('cd /usr/local/apps/marco/marco-portal/ && git fetch && git merge origin/master')
+
+def all():
+    """ Use all servers """
+    env.hosts = dev() + prod() + test()
+
+
+
+def _install_requirements():
+    run('cd %(app_dir)s && %(venv)s/bin/pip install -r ../requirements.txt' % vars)
+
+def _install_django():
+    run('cd %(app_dir)s && %(venv)s/bin/python manage.py syncdb --noinput && \
+                           %(venv)s/bin/python manage.py migrate --noinput' % vars)
+
+
+def init():
+    """ Initialize the forest planner application """
+    _install_requirements()
+    _install_django()
+
+
+def run_server():
+	run('cd /vagrant/marco && /usr/local/venv/marco/bin/python manage.py runserver 0.0.0.0:8000')
