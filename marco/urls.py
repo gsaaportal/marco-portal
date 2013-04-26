24b823e209d6785070b625adcee449591631f5f8
Tim Glaser <tim.glaser@ecotrust.org>
2013-04-26 10:39:44 -0700

    root path and /portal now go to home.html

--- marco/urls.py
+++ marco/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
     (r'^mobile/', include('visualize.urls')),
     (r'^feedback/', include('feedback.urls')),
     (r'^portal/', direct_to_template, {'template': 'home.html'}),
+    (r'^$', direct_to_template, {'template': 'home.html'}),
     (r'', include('madrona.common.urls')),
 )
 
