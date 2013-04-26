22dfd552eea263e42e665da2ada4170cc7d53b55
unknown <sfletcher@ecotrust.org>
2013-04-25 17:34:49 -0700

    removing the Data Priorities link for now. also adding landing page to /portal/ url.

--- marco/urls.py
+++ marco/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
     (r'^embed/', include('visualize.urls')),
     (r'^mobile/', include('visualize.urls')),
     (r'^feedback/', include('feedback.urls')),
-    (r'^$', direct_to_template, {'template': 'home.html'}),
+    (r'^portal/', direct_to_template, {'template': 'home.html'}),
     (r'', include('madrona.common.urls')),
 )
 
