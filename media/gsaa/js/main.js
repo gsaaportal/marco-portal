06d55355acfec3f0f0cf084b6dc29fddb1eadeeb
Tim Glaser <tim.glaser@ecotrust.org>
2013-04-29 10:09:43 -0700

    fixed IE 8 layout of .page-blocks

--- media/gsaa/js/main.js
+++ media/gsaa/js/main.js
@@ -59,35 +59,9 @@ $(document).ready(function () {
     }); 
 });
 
-// function setPageBlockSizes() {
-//     var maxHeight = -1;
-//     $('.page-blocks > [class*="row"]').each(function () {
-//         maxHeight = -1;
-//         $(this).find('[class*="span"]').each(function () {
-//             maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-//         });
-//         $(this).height(maxHeight);
-//         $(this).find('[class*="span"]').each(function () {
-//             $(this).height(maxHeight);
-//         });
-//     });
-// }
-
-
 /**
- * Set page block left margins to one pixel less due to border. 
- * todo: determine which span in each row is tallest and only use a border there to allow
- * for the borders to reach fully to the next row's top border.
+ * Home page carousel
  */
-$(document).ready(function () {
-    $('.page-blocks > [class*="row"] > [class*="span"]:first-child').css({'margin-right': '-=1'});
-    $('.page-blocks > [class*="row"] > [class*="span"]:last-child').css({'margin-left': '-=1'});
-
-    // setPageBlockSizes();
-    // $(window).resize(setPageBlockSizes);
-});
-
-/* home page carousel */
 $(document).ready(function () {
     $('#homeCarousel').carousel({
         interval: 4000
