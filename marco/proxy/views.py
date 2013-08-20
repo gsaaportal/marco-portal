c5ae2b813d6884e083ac0b6cfcf4034a176c689c
dramage <danramage@Dans-MacBook-Pro.local>
2013-08-20 13:56:35 -0400

    Added domain check.

--- marco/proxy/views.py
+++ marco/proxy/views.py
@@ -1,6 +1,6 @@
 import httplib2
 from urllib import urlencode
-from urlparse import urlparse 
+from urlparse import urlparse
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.http import HttpResponse
@@ -9,33 +9,40 @@ import logging
 import logging.config
 
 #PROXY_FORMAT = u"http://%s/%s" % (settings.PROXY_DOMAIN, u"%s")
-
+allowedDomain = "gsaaportal.org"
 def getLegendJSON(request, url):
-    logger = logging.getLogger(__name__)  
+    logger = logging.getLogger(__name__)
     logger.info("Begin getLegendJSON")
     logger.debug("Request: %s" % (request))
     conn = httplib2.Http()
     # optionally provide authentication for server
     #conn.add_credentials('admin','admin-password')
     if request.method == "GET":
-        
+
         getUrl = request.GET.get('url')
         logger.debug(getUrl)
         parsedURL = urlparse(getUrl)
-        #url_ending = "%s?%s" % (parts[0], parts[-1])
-        #print(url_ending)
-        #url = PROXY_FORMAT % url_ending
-        #resp, content = conn.request(url, request.method)
-        try:
-          results = requests.get(getUrl)
-        except Exception,e:
-          if(logger):
-            logger.exception(e)
+        if(parsedURL.hostname == allowedDomain):
+          try:
+            results = requests.get(getUrl)
+          except Exception,e:
+            if(logger):
+              logger.exception(e)
+          else:
+            if(results.status_code == 200):
+              return HttpResponse(results.text)
+            return(HttpResponse(''))
         else:
-          if(results.status_code == 200):          
-            return HttpResponse(results.text)
-          return(HttpResponse(''))
+          logger.error("Illegal domain request attempt!")
+          return(HttpResponse(status=403))
+
     elif request.method == "POST":
-        data = urlencode(request.POST)
-        resp, content = conn.request(url, request.method, data)
-        return HttpResponse(content)
+        parsedURL = urlparse(url)
+        if(parsedURL.hostname == allowedDomain):
+          data = urlencode(request.POST)
+          resp, content = conn.request(url, request.method, data)
+          return HttpResponse(content)
+        else:
+          return(HttpResponse(status=403))
+
+    return('')
