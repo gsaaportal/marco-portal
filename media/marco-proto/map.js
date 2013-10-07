92b13c3c5f576abb38e8647377fb71ae2cd468bd
dramage <danramage@Dans-MacBook-Pro.local>
2013-10-07 12:07:19 -0400

    Added the mouse position to the map to show lon/lat as mouse moves

--- media/marco-proto/map.js
+++ media/marco-proto/map.js
@@ -96,9 +96,9 @@ app.init = function () {
         }
     });
 
-    // map.addControl(new OpenLayers.Control.MousePosition({
-    //     element: document.getElementById('pos')
-    // }));
+    map.addControl(new OpenLayers.Control.MousePosition({
+      numDigits: 3
+      }));
 
     map.events.register("moveend", null, function () {
         // update the url when we move
