1d897c69ff6d8a3af5d05bd411e1aae4303cc852
DanRamage <dan@inlet.geol.sc.edu>
2014-08-19 11:23:37 -0400

    Trying to fix issue when Data tab has expanded all the themes and you try to expand a parent layer near the bottom the sublayers are cutoff from display.

--- media/marco-proto/models.js
+++ media/marco-proto/models.js
@@ -544,6 +544,7 @@ function layerModel(options, parent) {
     self.showSublayers.subscribe(function () {
         setTimeout(function () {
             if ( app.viewModel.activeLayer().subLayers.length > 1 ) {
+              app.viewModel.updateScrollBars();
                 //$('.layer').find('.open .layer-menu').jScrollPane();
             }
         });
